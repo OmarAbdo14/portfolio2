eac6ea982418e2367b4b114a6ccd683c5c282458
OmarAbdo14 <77195857+OmarAbdo14@users.noreply.github.com>
2021-08-07 23:15:05 +0200

    first-publish

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let image = document.getElementById('designer-image');
+let overlay = document.getElementById('overlay');
+
+image.addEventListener('click', () => {
+    overlay.style.display = 'grid';
+})
